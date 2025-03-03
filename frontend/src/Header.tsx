db03cd0a4b538bbf832596a59f323705123beb52
Freeman, Jabari <jabarifreeman@my.unt.edu>
2025-03-02 23:16:12 -0600

    Reverse engineered code for website, Created new page section and style.

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>My Knowledge Management System2</h1>
+                <p>Frontend is working!</p>
+        
+            </header>
+        </div>
+    );
+}
+export default Header;
